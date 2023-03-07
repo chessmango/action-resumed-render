29451b004bb05923b6202d1a480744de8d1eb69c
Owen Valentine <chessmango@users.noreply.github.com>
2023-03-07 18:05:08 +0200

    fix: npm install theme globally

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ INPUT_FILE="$2"
 OUTPUT_FILE="$3"
 
 echo "Installing theme: $THEME"
-npm install "$THEME"
+npm install -g "$THEME"
 
 resumed render --theme "$THEME" "$INPUT_FILE" --output "$OUTPUT_FILE"
